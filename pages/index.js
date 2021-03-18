0ffd66b9872e54983ae0a5c1e92cb3e5f1a0842a
Rodolfo Santana <rdmoedas@gmail.com>
2021-03-18 20:55:25 -0300

    add hello word to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,38 @@
-function home() {
+import { useState } from 'react';
+//Importação do useState para utilizar um componente com memória
+
+function Home() {
     return (
         <div>
-            <h1>Home Rodev.com.br :)</h1>
-            <contador />
-        
+            <h1>Hello World!</h1>
+            <div>
+                <p>Site em desenvolvimento.</p>
+                <a href="https://github.com/rdmoedas">Link do meu github</a>
+                <a href="https://www.linkedin.com/in/rodolfo-santana-57766b47/">Link do meu linkedin</a>
+            </div>
+            <Contador />
         </div>
-    
-
-
     )
 }
 
-function contador() {
+function Contador() {
     const [contador, setContador] = useState(1);
 
     function adicionarContador() {
         setContador(contador + 1);
     }
+    function subtrairContador() {
+        setContador(contador - 1);
+    }
 
     return (
         <div>
             <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
+            <div><button onClick={adicionarContador}>Adicionar</button></div>
+            <div><button onClick={subtrairContador}>Remover</button></div>
         </div>
     )
 }
 
 //Novo padrão de export de módulo do js
-export default home
+export default Home
